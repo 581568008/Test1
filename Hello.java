318dec245eebc0df23ed03d112e5b17b5cd75b2d
Withun saewue <withun.sae@crru.ac.th>
2016-11-21 10:45:13 +0700

    upgrde Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
 	public static void main (String args[]){
-		System.out.println("Hello World")
-		System.out.println("Hello Travis")
+		System.out.println("HelloWorld")
+		System.out.println("HelloTravis")
 	}
 }
